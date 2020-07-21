66e7291045e4437d42f6a30f1080cfda26c45211
Jang Jun Hyuk <dejavukk1@gmail.com>
2020-07-21 23:54:39 +0900

    업로드.
    알고리즘.

---
+++ ComputerScience.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) 
+{
+
+    int number[6] = {4, 8, 15, 36, 42, 48};
+
+    
+
+    for (int i = 0; i < 6; i++)
+    {
+        /* code */
+        if (number[i] == 50)
+        {
+            /* code */
+            printf("Found\n");
+        }
+        
+    }
+    printf("Not found\n");
+
+    printf("Hello C");
+}
